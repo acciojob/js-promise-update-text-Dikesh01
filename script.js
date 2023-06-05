3ce351a31147584896dd1d0defc73269308750ef
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-06-05 09:19:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const outPut = document.getElementById("output");
+
+function getResolve(){
+	new Promise((resolve) =>{
+		setTimeout(()=>{
+		resolve("Hello, world!")
+	},1000)
+	}).then(data =>{
+		outPut.innerText = data;
+	})	 
+}
+
+getResolve();
+
+
+
